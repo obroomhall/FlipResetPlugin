f9ae7257c459c7f42d04b5ef9cb04db9b2b6a5bf
Oliver Broomhall <obroomhall@gmail.com>
2021-06-19 12:13:36 +0100

    Display FLIP message in boost area

--- FlipResetPlugin.cpp
+++ FlipResetPlugin.cpp
@@ -1,15 +1,48 @@
 #include "FlipResetPlugin.h"
-#include "bakkesmod\wrappers\GameEvent\TutorialWrapper.h"
-#include "bakkesmod\wrappers\GameObject\BallWrapper.h"
-#include "bakkesmod\wrappers\GameObject\CarWrapper.h"
+#include "bakkesmod\wrappers\GameObject\CarComponent\CarComponentWrapper.h"
+#include "bakkesmod\wrappers\GameObject\CarComponent\DoubleJumpComponentWrapper.h"
+#include "bakkesmod\wrappers\GameObject\CarComponent\FlipCarComponentWrapper.h"
 
 BAKKESMOD_PLUGIN(FlipResetPlugin, "Flip Reset Plugin", "0.1", PLUGINTYPE_FREEPLAY)
 
-void FlipResetPlugin::onLoad() {
+void FlipResetPlugin::onLoad()
+{
+	gameWrapper->HookEvent("Function TAGame.Vehicle_TA.GetNumWheelWorldContacts", [this](std::string eventName) {
+		onWheelContact();
+	});
+
+	gameWrapper->RegisterDrawable(std::bind(&FlipResetPlugin::render, this, std::placeholders::_1));
+}
+
+void FlipResetPlugin::onUnload()
+{
 
 }
 
-void FlipResetPlugin::onUnload() {
+void FlipResetPlugin::onWheelContact()
+{
+	CarWrapper car = gameWrapper->GetLocalCar();
 
+	hasFlip = car.HasFlip();
 }
+
+void FlipResetPlugin::render(CanvasWrapper canvas)
+{
+	if (!gameWrapper->IsInGame() || gameWrapper->IsPaused())
+		return;
+	
+	Vector2 size = canvas.GetSize();
+
+	Vector2 drawLoc = { size.X * 0.90, size.Y * 0.895 };
+
+	canvas.SetPosition(drawLoc);
+	canvas.SetColor(255, 255, 255, 255);
+
+	if (hasFlip)
+	{
+		float scale = size.X / 853;
+		canvas.DrawString("FLIP", scale, scale);
+	}
+}
+
 
--- FlipResetPlugin.h
+++ FlipResetPlugin.h
@@ -5,6 +5,9 @@
 class FlipResetPlugin : public BakkesMod::Plugin::BakkesModPlugin
 {
 public:
+	bool hasFlip;
 	virtual void onLoad();
 	virtual void onUnload();
+	void onWheelContact();
+	void render(CanvasWrapper canvas);
 };
